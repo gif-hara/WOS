3f1b180e8dd5dee138958e16a3d88397c147f77e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-26 12:15:34 +0900

    ActorクラスにcharacterControllerフィールドを追加し、Awakeメソッドで初期化するように変更

--- WOS/Assets/WOS/Scripts/ActorControllers/Actor.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Actor.cs
@@ -1,8 +1,23 @@
+using StandardAssets.Characters.Physics;
 using UnityEngine;
 
 namespace WOS.ActorControllers
 {
     public sealed class Actor : MonoBehaviour
     {
+        [field: SerializeField]
+        private OpenCharacterController characterController;
+
+        public ActorMovementController MovementController { get; private set; }
+
+        public ActorBrainController BrainController { get; private set; }
+
+        private void Awake()
+        {
+            MovementController = new ActorMovementController(this, characterController);
+            BrainController = new ActorBrainController(this);
+
+            MovementController.Activate();
+        }
     }
 }
