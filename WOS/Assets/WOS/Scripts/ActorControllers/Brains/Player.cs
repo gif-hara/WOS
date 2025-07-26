c4114802a8c69252e60d776b004ae675e7100caa
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-26 15:02:52 +0900

    Playerクラスを新規作成し、Actorの動きを制御するActivateメソッドを実装

---
+++ WOS/Assets/WOS/Scripts/ActorControllers/Brains/Player.cs
@@ -0,0 +1,39 @@
+using System.Threading;
+using R3;
+using R3.Triggers;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace WOS.ActorControllers.Brains
+{
+    public class Player : IActorBrain
+    {
+        private readonly PlayerInput playerInput;
+
+        private readonly Camera camera;
+
+        public Player(PlayerInput playerInput, Camera camera)
+        {
+            this.playerInput = playerInput;
+            this.camera = camera;
+        }
+
+        public void Activate(Actor actor, CancellationToken cancellationToken)
+        {
+            actor.UpdateAsObservable()
+                .Subscribe((this, actor), static (_, t) =>
+                {
+                    var (@this, actor) = t;
+                    var moveInput = @this.playerInput.actions["Move"].ReadValue<Vector2>();
+                    var moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
+                    if (moveDirection != Vector3.zero)
+                    {
+                        moveDirection = @this.camera.transform.TransformDirection(moveDirection);
+                        moveDirection.y = 0; // Keep movement on the horizontal plane
+                        actor.MovementController.Move(moveDirection);
+                    }
+                })
+                .RegisterTo(actor.destroyCancellationToken);
+        }
+    }
+}
