10e251ba92b66b74441e6b784628d1af522377d0
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-26 12:10:37 +0900

    ActorMovementControllerスクリプトとメタファイルを新規作成

---
+++ WOS/Assets/WOS/Scripts/ActorControllers/ActorMovementController.cs
@@ -0,0 +1,36 @@
+using R3;
+using R3.Triggers;
+using StandardAssets.Characters.Physics;
+using UnityEngine;
+
+namespace WOS.ActorControllers
+{
+    public class ActorMovementController
+    {
+        private readonly Actor actor;
+
+        private readonly OpenCharacterController characterController;
+
+        private Vector3 velocity;
+
+        public ActorMovementController(Actor actor, OpenCharacterController characterController)
+        {
+            this.actor = actor;
+            this.characterController = characterController;
+        }
+
+        public void Activate()
+        {
+            actor.UpdateAsObservable()
+                .Subscribe(this, static (_, @this) =>
+                {
+                    if (@this.velocity != Vector3.zero)
+                    {
+                        @this.characterController.Move(@this.velocity);
+                        @this.velocity = Vector3.zero;
+                    }
+                })
+                .RegisterTo(actor.destroyCancellationToken);
+        }
+    }
+}
