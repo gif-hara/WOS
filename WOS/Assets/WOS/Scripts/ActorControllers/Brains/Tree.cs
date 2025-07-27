e3faf9df9d03ad092af2287902a8042b402282c5
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-27 14:25:05 +0900

    TreeクラスにIInteractionインターフェースを追加し、インタラクション機能を実装

--- WOS/Assets/WOS/Scripts/ActorControllers/Brains/Tree.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Brains/Tree.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using Cysharp.Threading.Tasks;
 using R3;
 using R3.Triggers;
 using UnityEngine;
@@ -7,7 +8,7 @@ using UnityEngine;
 namespace WOS.ActorControllers.Brains
 {
     [Serializable]
-    public sealed class Tree : IActorBrain
+    public sealed class Tree : IActorBrain, IInteraction
     {
         public void Activate(Actor actor, CancellationToken cancellationToken)
         {
@@ -15,11 +16,27 @@ namespace WOS.ActorControllers.Brains
 
             actor.Document.Q<Collider>("Trigger")
                 .OnTriggerEnterAsObservable()
-                .Subscribe(x =>
+                .Subscribe((this, actor), static (x, t) =>
                 {
-                    Debug.Log("Trigger entered by: " + x.name);
+                    var (@this, actor) = t;
+                    if (x.attachedRigidbody == null)
+                    {
+                        return;
+                    }
+                    if (!x.attachedRigidbody.TryGetComponent<Actor>(out var collidedActor))
+                    {
+                        return;
+                    }
+                    collidedActor.InteractionController.AddInteraction(@this);
                 })
                 .RegisterTo(cancellationToken);
+        }
+
+        public async UniTask InteractAsync(Actor actor, CancellationToken cancellationToken)
+        {
+            Debug.Log("Begin interaction with Tree");
+            await UniTask.Delay(1000, cancellationToken: cancellationToken); // Simulate some interaction delay
+            Debug.Log("Interaction with Tree completed");
         }
     }
 }
