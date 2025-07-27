b4cb06d5b86756ea19582cc879ab919a20693519
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-27 14:45:28 +0900

    Treeクラスにトリガーのエンターハンドリングを追加し、インタラクションの状態管理を改善

--- WOS/Assets/WOS/Scripts/ActorControllers/Brains/Tree.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Brains/Tree.cs
@@ -14,10 +14,18 @@ namespace WOS.ActorControllers.Brains
 
         public Transform Transform => actor.transform;
 
+        private GameObject SceneViewTree => actor.Document.Q("SceneView.Tree");
+
+        private GameObject SceneViewStump => actor.Document.Q("SceneView.Stump");
+
+        private Collider Trigger => actor.Document.Q<Collider>("Trigger");
+
         public void Activate(Actor actor, CancellationToken cancellationToken)
         {
             this.actor = actor;
-            actor.Document.Q<Collider>("Trigger")
+            SceneViewTree.SetActive(true);
+            SceneViewStump.SetActive(false);
+            Trigger
                 .OnTriggerEnterAsObservable()
                 .Subscribe((this, actor), static (x, t) =>
                 {
@@ -33,13 +41,42 @@ namespace WOS.ActorControllers.Brains
                     collidedActor.InteractionController.AddInteraction(@this);
                 })
                 .RegisterTo(cancellationToken);
+            Trigger
+                .OnTriggerExitAsObservable()
+                .Subscribe((this, actor), static (x, t) =>
+                {
+                    var (@this, actor) = t;
+                    if (x.attachedRigidbody == null)
+                    {
+                        return;
+                    }
+                    if (!x.attachedRigidbody.TryGetComponent<Actor>(out var collidedActor))
+                    {
+                        return;
+                    }
+                    collidedActor.InteractionController.RemoveInteraction(@this);
+                })
+                .RegisterTo(cancellationToken);
         }
 
         public async UniTask InteractAsync(Actor actor, CancellationToken cancellationToken)
         {
             Debug.Log("Begin interaction with Tree");
             await UniTask.Delay(1000, cancellationToken: cancellationToken); // Simulate some interaction delay
+            SceneViewTree.SetActive(false);
+            SceneViewStump.SetActive(true);
+            Trigger.enabled = false;
+            BeginRecoveryAsync(actor.destroyCancellationToken).Forget();
             Debug.Log("Interaction with Tree completed");
+        }
+
+        private async UniTask BeginRecoveryAsync(CancellationToken cancellationToken)
+        {
+            Debug.Log("Begin recovery from Tree interaction");
+            await UniTask.Delay(3000, cancellationToken: cancellationToken); // Simulate recovery delay
+            SceneViewTree.SetActive(true);
+            SceneViewStump.SetActive(false);
+            Trigger.enabled = true;
         }
     }
 }
