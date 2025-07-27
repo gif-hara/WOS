69924391c97437384d84396f61d981c6ea4c60aa
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-27 14:03:12 +0900

    ActorBrainAttacherクラスにActorフィールドを追加し、Startメソッドを実装

--- WOS/Assets/WOS/Scripts/ActorControllers/ActorBrainAttacher.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/ActorBrainAttacher.cs
@@ -7,6 +7,14 @@ namespace WOS.ActorControllers
     public class ActorBrainAttacher : MonoBehaviour
     {
         [field: SerializeField]
+        private Actor actor;
+
+        [field: SerializeField]
         private SerializableInterface<IActorBrain> brain;
+
+        void Start()
+        {
+            actor.BrainController.Change(brain.Value);
+        }
     }
 }
