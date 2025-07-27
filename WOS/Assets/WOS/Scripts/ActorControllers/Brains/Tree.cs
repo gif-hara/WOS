b50c70c1ead8f91d0f851a8ff858148f4cccec30
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-27 14:08:18 +0900

    Actor.Tree.1.prefabに新しいコンポーネントを追加し、Triggerの設定を更新

--- WOS/Assets/WOS/Scripts/ActorControllers/Brains/Tree.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Brains/Tree.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading;
+using R3;
+using R3.Triggers;
 using UnityEngine;
 
 namespace WOS.ActorControllers.Brains
@@ -10,6 +12,14 @@ namespace WOS.ActorControllers.Brains
         public void Activate(Actor actor, CancellationToken cancellationToken)
         {
             Debug.Log("Tree brain activated");
+
+            actor.Document.Q<Collider>("Trigger")
+                .OnTriggerEnterAsObservable()
+                .Subscribe(x =>
+                {
+                    Debug.Log("Trigger entered by: " + x.name);
+                })
+                .RegisterTo(cancellationToken);
         }
     }
 }
