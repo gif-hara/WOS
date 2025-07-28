d155dd0401c67d08c4138c9df2eac4d314ba0d07
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-28 23:18:23 +0900

    ActorAnimationControllerクラスのAnimatorフィールドとハッシュを追加

--- WOS/Assets/WOS/Scripts/ActorControllers/ActorAnimationController.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/ActorAnimationController.cs
@@ -4,5 +4,21 @@ namespace WOS.ActorControllers
 {
     public class ActorAnimationController
     {
+        private readonly Animator animator;
+
+        private readonly int
+            IdleHash = Animator.StringToHash("Idle"),
+            AttackHash = Animator.StringToHash("Attack");
+
+
+        public ActorAnimationController(Animator animator)
+        {
+            this.animator = animator;
+        }
+
+        public void RequestAttack()
+        {
+            animator.SetTrigger(AttackHash);
+        }
     }
 }
