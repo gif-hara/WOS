17328701462cca2b4177589095087cdfd8a6019e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-11 17:34:42 +0900

    Inventoryプロパティを公開し、Activateメソッド内で初期化するように修正

--- WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorInventory.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorInventory.cs
@@ -1,21 +1,12 @@
-using System.Collections.Generic;
-using UnityEngine;
-using WOS.MasterDataSystem;
-
 namespace WOS.ActorControllers.Abilities
 {
     public class ActorInventory : IActorAbility
     {
-        private Inventory inventory;
+        public Inventory Inventory { get; private set; }
 
         public void Activate(Actor actor)
         {
-            inventory = new Inventory(actor.Document.Q<PlacementPoint>("PlacementPoint"));
-        }
-
-        public void AddItems(List<Inventory.Element> elements)
-        {
-            inventory.AddItems(elements);
+            Inventory = new Inventory(actor.Document.Q<PlacementPoint>("PlacementPoint"));
         }
     }
 }
