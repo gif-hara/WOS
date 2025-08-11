dc1e81ceda055cc670ff3ed19bd14c37fb18f922
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-11 20:37:16 +0900

    TaskRunnerSpawnerクラスを新規作成し、タスクランナーの生成機能を追加

---
+++ WOS/Assets/WOS/Scripts/TaskRunnerSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using TNRD;
+using UnityEngine;
+using WOS.ActorControllers;
+using WOS.ActorControllers.Abilities;
+using WOS.ActorControllers.Brains;
+using WOS.ActorControllers.TaskSystems;
+
+namespace WOS
+{
+    public class TaskRunnerSpawner : MonoBehaviour
+    {
+        [field: SerializeField]
+        private Actor taskRunnerPrefab;
+
+        [field: SerializeField]
+        private List<SerializableInterface<ITask>> tasks;
+
+        void Start()
+        {
+            SpawnTaskRunner();
+        }
+
+        private void SpawnTaskRunner()
+        {
+            var taskRunner = Instantiate(taskRunnerPrefab);
+            taskRunner.AddAbility<ActorBrain>().Change(new TaskRunner(tasks.Select(x => x.Value)));
+        }
+    }
+}
