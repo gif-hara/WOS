b143ecb978387ac5144ca7ad5b4bbf0782010ca2
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-11 11:05:15 +0900

    非同期移動機能のキャンセルトークンを管理するためのコードを追加

--- WOS/Assets/WOS/Scripts/Item.cs
+++ WOS/Assets/WOS/Scripts/Item.cs
@@ -8,12 +8,18 @@ namespace WOS
 {
     public class Item : MonoBehaviour
     {
+        private CancellationTokenSource moveScope;
+
         public async UniTask BeginMoveAsync(Func<Vector3> toPositionSelector, Transform parent, CancellationToken cancellationToken)
         {
+            moveScope?.Cancel();
+            moveScope?.Dispose();
+            moveScope = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
             var fromPosition = transform.position;
             await LMotion.Create(0.0f, 1.0f, 0.5f)
                 .Bind(x => transform.position = Vector3.Lerp(fromPosition, toPositionSelector(), x))
-                .ToUniTask(cancellationToken);
+                .ToUniTask(moveScope.Token);
             transform.SetParent(parent);
             transform.localPosition = Vector3.zero;
             transform.localRotation = Quaternion.identity;
