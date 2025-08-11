c8902052a84161f8ee8e65a385413b678586ba77
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-11 19:58:28 +0900

    Columnクラスを新規作成し、子オブジェクトの状態管理機能を追加

---
+++ WOS/Assets/WOS/Scripts/Column.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WOS
+{
+    public class Column : MonoBehaviour
+    {
+        private readonly List<bool> isFilleds = new();
+
+        void Start()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                isFilleds.Add(false);
+            }
+        }
+
+        public void SetFilled(int index, bool isFilled)
+        {
+            if (index < 0 || index >= isFilleds.Count)
+            {
+                Debug.LogError($"Index {index} out of range for Column {name}");
+                return;
+            }
+            isFilleds[index] = isFilled;
+        }
+
+        public Transform Get(int index)
+        {
+            if (index < 0 || index >= transform.childCount)
+            {
+                Debug.LogError($"Index {index} out of range for Column {name}");
+                return null;
+            }
+            return transform.GetChild(index);
+        }
+
+        public int GetLastFilledIndex()
+        {
+            for (int i = isFilleds.Count - 1; i >= 0; i--)
+            {
+                if (isFilleds[i])
+                {
+                    return i;
+                }
+            }
+            return -1; // No filled column found
+        }
+    }
+}
