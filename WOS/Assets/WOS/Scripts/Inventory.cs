980c3275e7c19081564c156c883a29cb6ae62eeb
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-11 10:42:59 +0900

    Inventoryクラスにアイテム追加機能を実装し、要素の管理を強化

--- WOS/Assets/WOS/Scripts/Inventory.cs
+++ WOS/Assets/WOS/Scripts/Inventory.cs
@@ -1,12 +1,45 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using WOS.MasterDataSystem;
 
 namespace WOS
 {
     public class Inventory
     {
-        private readonly Vector3 originPosition;
+        private readonly PlacementPoint placementPoint;
 
-        private readonly Vector2 interval = new(1, 1);
+        private readonly List<Element> elements = new();
+
+        public Inventory(PlacementPoint placementPoint)
+        {
+            this.placementPoint = placementPoint;
+        }
+
+        public void AddItem(ItemSpec itemSpec, GameObject itemObject)
+        {
+            Assert.IsNotNull(itemSpec, "ItemSpec cannot be null.");
+            Assert.IsNotNull(itemObject, "ItemObject cannot be null.");
+
+            var element = new Element(itemSpec, itemObject);
+            var point = placementPoint.Points[elements.Count % placementPoint.Points.Count];
+            element.ItemObject.transform.SetParent(point);
+            element.ItemObject.transform.localPosition = new Vector3(0, elements.Count / placementPoint.Points.Count * 0.25f, 0);
+            element.ItemObject.transform.localRotation = Quaternion.identity;
+            elements.Add(element);
+        }
+
+        public class Element
+        {
+            public ItemSpec ItemSpec { get; }
+
+            public GameObject ItemObject { get; }
+
+            public Element(ItemSpec itemSpec, GameObject itemObject)
+            {
+                ItemSpec = itemSpec;
+                ItemObject = itemObject;
+            }
+        }
     }
 }
