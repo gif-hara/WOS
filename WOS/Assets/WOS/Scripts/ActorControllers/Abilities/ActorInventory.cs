ef0e4512692f017cff76912d332aa11618f9c4c6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-11 10:43:11 +0900

    ActorInventoryクラスにInventoryの初期化とアイテム追加機能を実装

--- WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorInventory.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorInventory.cs
@@ -1,10 +1,20 @@
+using UnityEngine;
+using WOS.MasterDataSystem;
+
 namespace WOS.ActorControllers.Abilities
 {
     public class ActorInventory : IActorAbility
     {
+        private Inventory inventory;
+
         public void Activate(Actor actor)
         {
+            inventory = new Inventory(actor.Document.Q<PlacementPoint>("PlacementPoint"));
+        }
 
+        public void AddItem(ItemSpec itemSpec, GameObject itemObject)
+        {
+            inventory.AddItem(itemSpec, itemObject);
         }
     }
 }
