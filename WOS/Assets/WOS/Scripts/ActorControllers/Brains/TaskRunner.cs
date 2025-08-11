6a0a4c63e7a1cb0fbd112c5cf99771a145bf8fd6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-11 20:03:12 +0900

    TaskRunnerクラスを新規作成し、Activateメソッドを追加

---
+++ WOS/Assets/WOS/Scripts/ActorControllers/Brains/TaskRunner.cs
@@ -0,0 +1,13 @@
+using System.Threading;
+using UnityEngine;
+
+namespace WOS.ActorControllers.Brains
+{
+    public sealed class TaskRunner : IActorBrain
+    {
+        public void Activate(Actor actor, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
