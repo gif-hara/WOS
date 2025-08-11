f860f29b352d66a3d7c0961f097b03b605ead444
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-11 09:46:28 +0900

    Inventoryクラスを新規作成し、originPositionとintervalフィールドを追加

---
+++ WOS/Assets/WOS/Scripts/Inventory.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WOS
+{
+    public class Inventory
+    {
+        private readonly Vector3 originPosition;
+
+        private readonly Vector2 interval = new(1, 1);
+    }
+}
