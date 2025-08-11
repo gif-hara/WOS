3eb1fb8aab1291f9f1420a48b2f1538341a00bf7
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-11 11:27:06 +0900

    移動処理を改善し、Y軸の補間を追加して回転を親オブジェクトに合わせるように変更

--- WOS/Assets/WOS/Scripts/Item.cs
+++ WOS/Assets/WOS/Scripts/Item.cs
@@ -17,8 +17,53 @@ namespace WOS
             moveScope = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
 
             var fromPosition = transform.position;
-            await LMotion.Create(0.0f, 1.0f, 0.5f)
-                .Bind(x => transform.position = Vector3.Lerp(fromPosition, toPositionSelector(), x))
+            var yMax = Mathf.Max(fromPosition.y, toPositionSelector().y) + 2.0f;
+            var fromRotation = transform.rotation;
+            await LSequence.Create()
+                .Join(
+                    LMotion.Create(0.0f, 1.0f, 0.5f)
+                        .Bind(x =>
+                        {
+                            var toPosition = toPositionSelector();
+                            var position = transform.position;
+                            position.x = Mathf.Lerp(fromPosition.x, toPosition.x, x);
+                            position.z = Mathf.Lerp(fromPosition.z, toPosition.z, x);
+                            transform.position = position;
+                        })
+                )
+                .Join(
+                    LSequence.Create()
+                        .Append(
+                            LMotion.Create(0.0f, 1.0f, 0.25f)
+                                .WithEase(Ease.OutQuad)
+                                .Bind(x =>
+                                {
+                                    var toPosition = toPositionSelector();
+                                    var position = transform.position;
+                                    position.y = Mathf.Lerp(fromPosition.y, yMax, x);
+                                    transform.position = position;
+                                })
+                        )
+                        .Append(
+                            LMotion.Create(0.0f, 1.0f, 0.25f)
+                                .WithEase(Ease.InQuad)
+                                .Bind(x =>
+                                {
+                                    var position = transform.position;
+                                    position.y = Mathf.Lerp(yMax, toPositionSelector().y, x);
+                                    transform.position = position;
+                                })
+                        )
+                        .Run()
+                )
+                .Join(
+                    LMotion.Create(0.0f, 1.0f, 0.5f)
+                        .Bind(x =>
+                        {
+                            transform.rotation = Quaternion.Lerp(fromRotation, parent.rotation, x);
+                        })
+                )
+                .Run()
                 .ToUniTask(moveScope.Token);
             transform.SetParent(parent);
             transform.position = toPositionSelector();
