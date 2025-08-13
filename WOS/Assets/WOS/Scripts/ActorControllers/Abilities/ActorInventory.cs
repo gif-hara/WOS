afc4569551792f6bc7ab24223a985f9785ce555c
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-13 09:02:06 +0900

    ActorInventoryにお金の管理機能を追加

--- WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorInventory.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorInventory.cs
@@ -4,9 +4,27 @@ namespace WOS.ActorControllers.Abilities
     {
         public Inventory Inventory { get; private set; }
 
+        public int Money { get; private set; }
+
         public void Activate(Actor actor)
         {
             Inventory = new Inventory(actor.Document.Q<PlacementPoint>("PlacementPoint"));
+            Money = 0;
+        }
+
+        public void AddMoney(int amount)
+        {
+            Money += amount;
+        }
+
+        public void ConsumeMoney(int amount)
+        {
+            Money -= amount;
+        }
+
+        public bool IsEnoughMoney(int amount)
+        {
+            return Money >= amount;
         }
     }
 }
