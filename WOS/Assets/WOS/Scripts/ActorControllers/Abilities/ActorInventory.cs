ef4812aac87c4f48ac4f943326a3d7df657925d5
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-13 09:36:38 +0900

    ActorInventoryクラスのMoneyプロパティをReactivePropertyに変更

--- WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorInventory.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorInventory.cs
@@ -1,30 +1,35 @@
+using R3;
+
 namespace WOS.ActorControllers.Abilities
 {
     public class ActorInventory : IActorAbility
     {
         public Inventory Inventory { get; private set; }
 
-        public int Money { get; private set; }
+        private readonly ReactiveProperty<int> money = new(0);
+
+        public ReadOnlyReactiveProperty<int> MoneyAsObservable() => money;
+
+        public int Money => money.Value;
 
         public void Activate(Actor actor)
         {
             Inventory = new Inventory(actor.Document.Q<PlacementPoint>("PlacementPoint"));
-            Money = 0;
         }
 
         public void AddMoney(int amount)
         {
-            Money += amount;
+            money.Value += amount;
         }
 
         public void ConsumeMoney(int amount)
         {
-            Money -= amount;
+            money.Value -= amount;
         }
 
         public bool IsEnoughMoney(int amount)
         {
-            return Money >= amount;
+            return money.Value >= amount;
         }
     }
 }
