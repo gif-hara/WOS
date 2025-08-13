5402a4f862d39132e096e1d2835f0c9d6e9770f4
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-13 11:29:57 +0900

    UpgradeクラスにIInteractionインターフェースを追加し、アクターのアクティベート処理を修正

--- WOS/Assets/WOS/Scripts/ActorControllers/Brains/Upgrade.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Brains/Upgrade.cs
@@ -1,11 +1,46 @@
+using System.Collections.Generic;
 using System.Threading;
+using Cysharp.Threading.Tasks;
+using R3;
+using TNRD;
+using UnityEngine;
+using WOS.ActorControllers.UpgradeActions;
+using WOS.ActorControllers.UpgradeCosts;
 
 namespace WOS.ActorControllers.Brains
 {
-    public sealed class Upgrade : IActorBrain
+    public sealed class Upgrade : IActorBrain, IInteraction
     {
+        [field: SerializeField]
+        private SerializableInterface<IUpgradeCost> cost;
+
+        [field: SerializeField]
+        private List<SerializableInterface<IUpgradeAction>> actions;
+
+        private Actor actor;
+
+        public Transform Transform => actor.transform;
+
         public void Activate(Actor actor, CancellationToken cancellationToken)
         {
+            this.actor = actor;
+            this.SubscribeOnTrigger(actor, actor.Document.Q<Collider>("Trigger"))
+                .RegisterTo(cancellationToken);
+            cost.Value.BeginObserveView(cancellationToken);
+        }
+
+        public UniTask InteractAsync(Actor interactedActor, CancellationToken cancellationToken)
+        {
+            if (cost.Value.IsEnough())
+            {
+                foreach (var action in actions)
+                {
+                    action.Value.Execute();
+                }
+                cost.Value.Consume();
+                actor.gameObject.SetActive(false);
+            }
+            return UniTask.CompletedTask;
         }
     }
 }
