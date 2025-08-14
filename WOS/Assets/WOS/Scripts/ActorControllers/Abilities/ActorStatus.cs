6a49456f85ddafbf1d995f4132fb3b0cb0595480
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-14 14:25:38 +0900

    ActorStatusクラスにダメージ処理機能を追加

--- WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorStatus.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorStatus.cs
@@ -1,3 +1,5 @@
+using Cysharp.Threading.Tasks;
+
 namespace WOS.ActorControllers.Abilities
 {
     public class ActorStatus : IActorAbility
@@ -11,12 +13,28 @@ namespace WOS.ActorControllers.Abilities
         public ActorStatus(int hitPoint)
         {
             hitPointMax = hitPoint;
-            hitPoint = hitPointMax;
+            this.hitPoint = hitPointMax;
         }
 
         public void Activate(Actor actor)
         {
             this.actor = actor;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (hitPoint <= 0)
+            {
+                return;
+            }
+
+            hitPoint -= damage;
+            actor.Router.PublishAsync(new ActorEvent.OnTakeDamage(damage)).AsUniTask().Forget();
+            if (hitPoint <= 0)
+            {
+                hitPoint = 0;
+                actor.Router.PublishAsync(new ActorEvent.OnDie()).AsUniTask().Forget();
+            }
         }
     }
 }
