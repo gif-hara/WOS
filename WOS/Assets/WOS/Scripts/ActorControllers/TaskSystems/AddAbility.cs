7aa1e9637178075674393ae9184d0aa0f0554221
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-14 10:02:01 +0900

    AddAbilityクラスを新規作成し、能力追加機能を実装

---
+++ WOS/Assets/WOS/Scripts/ActorControllers/TaskSystems/AddAbility.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using WOS.ActorControllers.Abilities;
+
+namespace WOS.ActorControllers.TaskSystems
+{
+    [Serializable]
+    public sealed class AddAbility : ITask
+    {
+        [field: SerializeField]
+        private Define.ActorAbilityType[] abilityTypes;
+
+        public UniTask RunAsync(Actor actor, CancellationToken cancellationToken)
+        {
+            foreach (var abilityType in abilityTypes)
+            {
+                switch (abilityType)
+                {
+                    case Define.ActorAbilityType.Movement:
+                        actor.AddAbility<ActorMovement>();
+                        break;
+                    case Define.ActorAbilityType.Interaction:
+                        actor.AddAbility<ActorInteraction>();
+                        break;
+                    case Define.ActorAbilityType.Animation:
+                        actor.AddAbility<ActorAnimation>();
+                        break;
+                    case Define.ActorAbilityType.Inventory:
+                        actor.AddAbility<ActorInventory>();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(abilityType), abilityType, null);
+                }
+            }
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
