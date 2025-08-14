1793a88a22c29e4e06ce7eafa4bb47eb4865b07e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-14 15:37:46 +0900

    ActorAnimationクラスに攻撃待機機能を追加

--- WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorAnimation.cs
+++ WOS/Assets/WOS/Scripts/ActorControllers/Abilities/ActorAnimation.cs
@@ -1,3 +1,8 @@
+using System.Linq;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using R3;
+using R3.Triggers;
 using UnityEngine;
 
 namespace WOS.ActorControllers.Abilities
@@ -5,6 +10,8 @@ namespace WOS.ActorControllers.Abilities
     public class ActorAnimation : IActorAbility
     {
         private Animator animator;
+
+        private ObservableStateMachineTrigger[] stateMachineTriggers;
 
         private readonly int
             IdleHash = Animator.StringToHash("Idle"),
@@ -14,11 +21,19 @@ namespace WOS.ActorControllers.Abilities
         public void Activate(Actor actor)
         {
             animator = actor.Document.Q<Animator>("Animator");
+            stateMachineTriggers = animator.GetBehaviours<ObservableStateMachineTrigger>();
         }
 
         public void RequestAttack()
         {
             animator.SetTrigger(AttackHash);
+        }
+
+        public UniTask WaitForAttackAsync(CancellationToken cancellationToken)
+        {
+            return stateMachineTriggers.Select(x => x.OnStateExitAsObservable().Where(y => y.StateInfo.IsName("Attack")))
+                .Merge()
+                .FirstAsync(cancellationToken: cancellationToken).AsUniTask();
         }
     }
 }
