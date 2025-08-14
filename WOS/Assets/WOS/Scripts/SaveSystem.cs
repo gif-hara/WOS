f2a51a00c95ded4b6e977e2e4a1bb41503866098
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-14 21:45:24 +0900

    SaveSystemクラスを新規作成し、データの保存、読み込み、存在確認、削除機能を実装

---
+++ WOS/Assets/WOS/Scripts/SaveSystem.cs
@@ -0,0 +1,47 @@
+using System.IO;
+using UnityEngine;
+
+namespace MH3
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class SaveSystem
+    {
+        public static void Save<T>(T data, string path)
+        {
+            var json = JsonUtility.ToJson(data);
+            Debug.Log($"Saving data to {path}: {json}");
+            var encryptedJson = EncryptionUtility.Encrypt(json);
+            path = Application.persistentDataPath + "/" + path;
+            File.WriteAllText(path, encryptedJson);
+#if UNITY_WEBGL && !UNITY_EDITOR
+            NativeCaller.SyncFile();
+#endif
+        }
+
+        public static T Load<T>(string path)
+        {
+            if (!Contains(path))
+            {
+                return default;
+            }
+            path = Application.persistentDataPath + "/" + path;
+            var encryptedJson = File.ReadAllText(path);
+            var json = EncryptionUtility.Decrypt(encryptedJson);
+            return JsonUtility.FromJson<T>(json);
+        }
+
+        public static bool Contains(string path)
+        {
+            path = Application.persistentDataPath + "/" + path;
+            return File.Exists(path);
+        }
+
+        public static void Delete(string path)
+        {
+            path = Application.persistentDataPath + "/" + path;
+            File.Delete(path);
+        }
+    }
+}
