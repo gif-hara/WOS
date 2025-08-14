596a1ff6be93511a01d9bab28da27311f766a13d
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-14 09:58:08 +0900

    Defineクラスを新規作成し、ActorAbilityType列挙型を追加

---
+++ WOS/Assets/WOS/Scripts/Define.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace WOS
+{
+    public static class Define
+    {
+        public enum ActorAbilityType
+        {
+            Movement,
+            Interaction,
+            Animation,
+            Inventory
+        }
+    }
+}
