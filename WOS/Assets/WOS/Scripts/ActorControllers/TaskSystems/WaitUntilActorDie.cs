5315eaa2b206a0717564c8f1590353b5e447a225
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-14 15:54:07 +0900

    WaitUntilActorDieクラスを新規作成し、Actorの死亡イベントを待機する機能を実装

---
+++ WOS/Assets/WOS/Scripts/ActorControllers/TaskSystems/WaitUntilActorDie.cs
@@ -0,0 +1,19 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using R3;
+using UnityEngine;
+using VitalRouter.R3;
+
+namespace WOS.ActorControllers.TaskSystems
+{
+    public sealed class WaitUntilActorDie : ITask
+    {
+        [field: SerializeField]
+        private Actor target;
+
+        public UniTask RunAsync(Actor actor, CancellationToken cancellationToken)
+        {
+            return target.Router.AsObservable<ActorEvent.OnDie>().FirstAsync(cancellationToken: cancellationToken).AsUniTask();
+        }
+    }
+}
